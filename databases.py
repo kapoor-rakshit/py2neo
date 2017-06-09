03aba2047bfccbb5b7648ff4e6651704f3a87014
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2017-06-09 17:22:00 +0530

    Create databases.py

--- databases.py
+++ databases.py
@@ -14,3 +14,22 @@ for i in listofdict:
 	print(i)
 
 gr.run("match (k:masterchef{age:22}) detach delete k")
+
+                                                                                      #get data from csv file to neo4j database
+with open('C:\\Users\\R6000670\\Documents\\Neo4j\\pracneo\\import\\dataset.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        tt=row['Ticket_Type']
+        sv=row['Severity']
+        rqid=row['Requester_ID']
+        rs=row['Requester_Seniority']
+        fa=row["Filed_Against"]
+        pr=row["Priority"]
+        ito=row["IT_Owner_ID"]
+        do=row["Days_Open"]
+        sat=row["Satisfaction"]
+        tx = gr.begin()
+        a = Node("data",ticket_type=tt, severity=sv, request_id=rqid, requester_seniority=rs, filed_against=fa, priority=pr, it_owner_id=ito, days_open=do,satisfaction=sat)
+        tx.merge(a)
+        tx.commit()
+	
